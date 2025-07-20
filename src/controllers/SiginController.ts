1212b4da9850b433e0a599cfc600e694c4708a95
Mario Alves <marioalvesneto@hotmail.com>
2025-07-20 13:52:19 -0300

    feat: Zod validations

---
+++ src/controllers/SiginController.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { HttpRequest, HttpResponse } from "../types/Http";
+import { badRequest, created, ok } from "../utils/http";
+
+const schema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+export class SignInController {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
+
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    return created({
+      data,
+    });
+  }
+}
